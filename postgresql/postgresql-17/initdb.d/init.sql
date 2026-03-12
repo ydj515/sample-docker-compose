11de805491adf0b9a107db5f3350b63b9c475899
ydj515 <ydj515@hanmail.net>
2026-03-13 03:26:12 +0900

    feat: add .env & conf file in posgresql

--- postgresql/postgresql-17/initdb.d/init.sql
+++ postgresql/postgresql-17/initdb.d/init.sql
@@ -1,5 +1,13 @@
--- 'appdb'에 대한 권한을 가진 'myuser' 사용자를 생성합니다.
-CREATE USER myuser WITH PASSWORD 'mypassword';
-GRANT ALL PRIVILEGES ON DATABASE appdb TO myuser;
+-- 현재 접속 중인 데이터베이스 이름을 psql 변수로 저장합니다.
+-- docker-entrypoint 는 POSTGRES_DB 로 지정된 DB에 접속해 이 SQL 을 실행합니다.
+SELECT current_database() AS app_db_name \gset
 
--- 필요 시, 초기 테이블 생성 및 데이터 삽입 쿼리를 추가할 수 있습니다.
+-- 애플리케이션용 계정을 생성합니다.
+-- 샘플 구성에서는 단순성을 위해 계정명/비밀번호를 고정합니다.
+CREATE USER myuser WITH PASSWORD 'mypassword';
+
+-- 애플리케이션 계정에 데이터베이스 권한을 부여합니다.
+GRANT ALL PRIVILEGES ON DATABASE :"app_db_name" TO myuser;
+
+-- PostgreSQL 15+ 에서는 public 스키마 CREATE 권한도 함께 주는 편이 안전합니다.
+GRANT USAGE, CREATE ON SCHEMA public TO myuser;
